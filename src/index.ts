5f633f63f15018d5b2f0e0c80d021a05a6c8df6a
gegehprast <gegeh.prast@gmail.com>
2020-07-24 13:20:25 +0800

    initital: typescript, tslint, express, npm run all, dotenv, hello world

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+
+app.listen(process.env.APP_PORT || 3000)
